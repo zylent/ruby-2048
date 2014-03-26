6aece87ffa8f66776e9c7a8e670a1cf048005561
Austin Nason <anason@summitpolymers.com>
2014-03-26 13:07:10 -0400

    changed how i’m checking for the 0’s… still working on this…

--- 2048.rb
+++ 2048.rb
@@ -42,115 +42,43 @@ def moveleft(grid)
 
 end
 
-def moveright(grid)
+def moveright(grid, dir)
 	done = false
-		x = -1
-		y = 0
-	until done do
-		x += 1
+	diry = 0
+	dirx = 0
 
-		if y == 3 && x == 3
-			done = true
-		end
-
-		#print "x: #{x} "
-		#print "y: #{y} "
-		#puts ""
-
-		if grid[y][x] != 0
-		#puts ""
-		#puts grid[y][x]
-		n = grid[y][x]
-		end
-
-		if grid[y][x] == 0
-			grid[y][x] = n
-			puts n
-			grid[y-1][x-1] = 0
-		end
-		
-
-		if x == 3
-			y += 1
-			x = -1
-		end
-
+	if dir == "up"
+		diry = -1
+	elsif dir == "down"
+		diry = 1
+	elsif dir == "left"
+		dirx = -1
+	elsif dir == "right"
+		dirx = 1
 	end
+
+for y in 0..3 do
+	for x in 0..3 do
+		#this doesn't work, research how to do this in ruby
+		if ( 0 <= y <= 3 && 0 <= x <= 3) && grid[y+diry][x+dirx] = 0
+			puts "ok"
+
+		end
+	end
+end
 end
 
 
+dir = "right"
 display(grid)
 spawn(grid)
 puts ""
 display(grid)
-moveright(grid)
+#moveright(grid, dir)
 puts ""
 display(grid)
 
 
-
-
-#ALSO
-
-
-#ruby 2048, for fun and to learn ruby better
-
-grid = [ [1, 0, 0, 1], [0, 2, 0, 0], [0, 0, 0, 3], [1, 0, 0, 0] ]
-
-def display(grid)
-
-	for y in 0..3 do
-
-		for x in 0..3 do
-			print "#{grid[y][x]} "
-		end
-
-		puts 
-	end
-end
-
-#todo: make random number spawning do more than just 2 and 4, 
-#make it take into account the total value of the array
-def spawn(grid)
-
-	done = false
-
-	until done do
-		x = rand(4)
-		y = rand(4)
-		n = rand(1..2) * 2
-
-			if grid[y][x] == 0
-				grid[y][x] = n
-				done = true
-			end
-	end
-end
-
-#broken ignore
-def moveleft(grid)
-	grid.each do |x|
-		x.each do |y|
-			print grid[y][x]
-		end
-	end
-
-end
-
-def moveright(grid)
-	
-	for y in 0..3 do
-
-		for x in 0..3 do
-			n = grid[y][x]
-			if ( y < 3 and n == grid[y][x+1] ) || ( x < 3 && n == grid[y+1][x] )
-				print "ok"
-			end
-		end
-	end
-
-
-end
 
 
 
